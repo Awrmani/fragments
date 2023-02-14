84a6ff494bbbbf799f542111d79f48cdf83b5f1a
Arman Valaee <armanvalaee@gmail.com>
2023-02-13 21:34:31 -0500

    added post.js and post unit test

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,6 +1,9 @@
 // src/routes/api/post.js
+
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 const { Fragment } = require('../../model/fragment');
+
+//const logger = require('../../logger');
 
 module.exports = async (req, res) => {
   try {
@@ -11,11 +14,11 @@ module.exports = async (req, res) => {
     });
     await fragment.save();
     await fragment.setData(req.body);
-    if (!process.env.API_URL) {
-      res.setHeader('Location', req.headers.host + '/v1/fragments/' + fragment.id);
-    } else {
-      res.setHeader('Location', process.env.API_URL + '/v1/fragments/' + fragment.id);
-    }
+
+    res.setHeader(
+      'Location',
+      process.env.API_URL || req.headers.host + '/v1/fragments/' + fragment.id
+    );
     res.setHeader('content-type', fragment.type);
 
     return res.status(201).json(createSuccessResponse({ fragment }));
--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -1,44 +1,40 @@
-// tests/unit/get.test.js
+// tests/unit/post.test.js
 
 const request = require('supertest');
-
 const app = require('../../src/app');
 
-beforeAll(async () => {
-  await request(app)
-    .post('/v1/fragments')
-    .auth('user1@email.com', 'password1')
-    .set({ 'Content-Type': 'text/plain' })
-    .send('test');
-});
+const logger = require('../../src/logger');
 
-describe('GET /v1/fragments', () => {
+describe('POST /v1/fragments', () => {
   // If the request is missing the Authorization header, it should be forbidden
-  test('unauthenticated requests are denied', () => request(app).get('/v1/fragments').expect(401));
+  test('unauthenticated requests are denied', () => request(app).post('/v1/fragments').expect(401));
 
   // If the wrong username/password pair are used (no such user), it should be forbidden
   test('incorrect credentials are denied', () =>
-    request(app).get('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
+    request(app).post('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
 
-  // Using a valid username/password pair should give a success result with a .fragments array with length 1
-  test('authenticated users get a fragments array with certain length (not expand', async () => {
-    const res = await request(app).get('/v1/fragments').auth('user1@email.com', 'password1');
-    expect(res.statusCode).toBe(200);
-    expect(res.body.status).toBe('ok');
-    expect(Array.isArray(res.body.fragments)).toBe(true);
-    expect(res.body.fragments.length).toBe(1);
-    expect(typeof res.body.fragments[0] === 'string').toBe(true);
+  // Using valid credentials, but wrong type should return error 415 (wrong data type)
+  test('unsupported type', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .set('content-type', 'image/png')
+      .send(Buffer.from('test'))
+      .auth('user1@email.com', 'password1');
+    logger.debug(async () => {
+      const res = await request(app).get('/v1/fragments').auth('user1@email.com', 'password1');
+      res;
+    }, 'Check user posting fragment');
+    expect(res.statusCode).toBe(415);
   });
 
-  test('authenticated users get a fragments array with certain length (expand)', async () => {
+  // Using valid credentials and supported type should return 201 (record created)
+  test('authenticated users create text/plain fragment', async () => {
     const res = await request(app)
-      .get('/v1/fragments/?expand=1')
-      .query({ expand: '1' })
+      .post('/v1/fragments')
+      .set('content-type', 'text/plain')
+      .send(Buffer.from('test'))
       .auth('user1@email.com', 'password1');
-    expect(res.statusCode).toBe(200);
-    expect(res.body.status).toBe('ok');
-    expect(Array.isArray(res.body.fragments)).toBe(true);
-    expect(res.body.fragments.length).toBe(1);
-    expect(typeof res.body.fragments[0] === 'object').toBe(true);
+    expect(res.statusCode).toBe(201);
+    expect(res.headers.location).toBeDefined();
   });
 });
