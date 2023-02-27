cf901632921ec3b80ca6acf73074ae13a9bbc280
Arman Valaee <armanvalaee@gmail.com>
2023-02-27 01:16:49 -0500

    Updated ci.yml to build and push to docker hub

--- src/routes/api/get:id.js
+++ src/routes/api/get:id.js
@@ -9,12 +9,9 @@ const logger = require('../../logger');
  * Get a list of fragments for the given Id
  */
 module.exports = async (req, res) => {
-  console.log('----HERE----');
   try {
     logger.debug(req.params.id, '---ID---');
-    console.log('___ID___' + req.params.id);
     const fragment = await Fragment.byId(req.user, req.params.id);
-    console.log('___ID___2' + req.params.id);
     logger.debug({ fragment }, 'By ID');
     res.status(200).json(createSuccessResponse({ fragment }));
   } catch (err) {
