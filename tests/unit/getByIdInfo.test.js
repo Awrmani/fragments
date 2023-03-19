b11b1c4ff51eac6ab3a0ffd20cc4f8a7eb78b62f
Arman Valaee Asr <85237160+Awrmani@users.noreply.github.com>
2023-03-19 20:15:15 +0000

    Authentication tests added for get /:id/info

--- tests/unit/getByIdInfo.test.js
+++ tests/unit/getByIdInfo.test.js
@@ -6,6 +6,34 @@ const app = require('../../src/app');
 const logger = require('../../src/logger');
 
 describe('GET /v1/fragments/:id/info', () => {
+  // If the request is missing the Authorization header, it should be forbidden
+  test('unauthenticated requests are denied', async () => {
+    const fragment = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set({ 'content-type': 'text/html' })
+      .send('test');
+
+    const id = fragment.header.location.split('/')[3];
+    await request(app).get(`/v1/fragments/${id}/info`).expect(401);
+  });
+
+  // If the wrong username/password pair are used (no such user), it should be forbidden
+  test('incorrect credentials are denied', async () => {
+    const fragment = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set({ 'content-type': 'text/html' })
+      .send('test');
+
+    const id = fragment.header.location.split('/')[3];
+
+    request(app)
+      .get(`/v1/fragments/${id}/info`)
+      .auth('invalid@email.com', 'incorrect_password')
+      .expect(401);
+  });
+
   test('fragments/:id/info returns an existing fragment metadata', async () => {
     const fragment = await request(app)
       .post('/v1/fragments')
@@ -18,7 +46,6 @@ describe('GET /v1/fragments/:id/info', () => {
     const res = await request(app)
       .get(`/v1/fragments/${id}/info`)
       .auth('user1@email.com', 'password1');
-    expect(res.body.fragment).toBe('test');
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('ok');
   });
